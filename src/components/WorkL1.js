d531458177c4595e9769d3432f8f507c82c38831
Cameron Chilton <cameron.chilton@ablevets.com>
2021-02-19 13:59:50 -0500

    Work left 1 rotating images, php db working to pull items

--- src/components/WorkL1.js
+++ src/components/WorkL1.js
@@ -5,29 +5,74 @@ class WorkL1 extends Component {
   constructor() {
     super();
     this.state = {
-      work1data: {},
+      work1data: null,
+      work1title: '',
+      work1img: '',
+      newSampleUp: true,
     };
   }
 
   componentDidMount() {
+    this.fetchWorkData();
+  }
 
-
-    setTimeout( () => {
-      fetch('../php/getRandom.php')
-        .then(response => response.json())
-        .then(data => this.setState({work1data: data}));
-    }, 1500);
-    console.log('work1data:' + JSON.stringify(this.state.work1data));
-
+  componentDidUpdate() {
+    if (this.state.work1data !== null && this.state.newSampleUp) {
+      this.addWorkData(this.state.work1data);
+    }
 
   }
 
+  fetchWorkData = () => {
+
+    const url = 'http://localhost/php/getRandom.php';
+    setTimeout( () => {
+      fetch(url)
+        .then(response => response.json())
+        .then(data => this.setState({work1data: data}));
+        this.setState({newSampleUp: true});
+    }, 1);
+    setInterval( () => {
+      fetch(url)
+        .then(response => response.json())
+        .then(data => this.setState({work1data: data}));
+        this.setState({newSampleUp: true});
+    }, 16000);
+
+  }
+
+  addWorkData = (data) => {
+
+    const workSample = Object.values(data);
+    //console.log('addWorkData workSample: ' + JSON.stringify(workSample));
+
+    const workLev1 = workSample[0];
+    //console.log('workLev1: ' + JSON.stringify(workLev1));
+
+    // title
+    this.setState({work1title: workLev1.title});
+    //console.log('this.state.work1title: ' + this.state.work1title);
+
+    // img url
+    this.setState({work1img: workLev1.img_url});
+    //console.log('this.state.work1img: ' + this.state.work1img);
+
+    // set up to false
+    this.setState({newSampleUp: false});
+  }
+
   render() {
+
+    //console.log('work1data:' + JSON.stringify(this.state.work1data));
+
     return (
       <div className="workL1">
-        <div className="contentL1"></div>
+        <div className="contentL1">
+          {this.state.work1img && <img src={this.state.work1img} />}
+        </div>
       </div>
     );
+
   }
 
 }
--- src/php/connect.php
+++ src/php/connect.php
@@ -1,5 +1,4 @@
 <?php
-
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
 header("Access-Control-Allow-Headers: X-Requested-With");
@@ -16,7 +15,7 @@ try {
     $DB = new PDO("mysql:host=$servername;dbname=ccsite2K21", $username, $password);
     // set the PDO error mode to exception
     $DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
+    //echo "Connected successfully";
     }
 catch(PDOException $e)
     {
